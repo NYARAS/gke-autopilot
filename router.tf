4bacea54d298efb1acfe9164e6ef4ac004eb2b08
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-22 16:00:11 +0300

    chore: add firewall config for both web and database
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- router.tf
+++ router.tf
@@ -1,4 +1,4 @@
 resource "google_compute_router" "web" {
-  name = "web"
+  name    = "web"
   network = google_compute_network.custom.id
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "region" {
   default     = "europe-west1"
   description = "GCP region to create the resources"
 }
+
+variable "authorized_source_ranges" {
+  type        = list(string)
+  description = "Addresses or CIDR blocks which are allowed to connect to GKE API Server"
+}
