6f40790eafe2cd8fb7196a2f5d07f7d00cfca007
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-23 13:54:12 +0300

    chore: add provider gcp project id variable
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "authorized_source_ranges" {
   type        = list(string)
   description = "Addresses or CIDR blocks which are allowed to connect to GKE API Server"
 }
+
+variable "project_id" {
+  type        = string
+  description = "GCP project id to create the resources"
+}
